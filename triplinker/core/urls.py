261cf129527bcc7dbc9c120c4d288161195b211f
Alex Rozum <rozumalex@gmail.com>
2020-07-17 13:57:33 +0300

    Add login/signup pages
    Add templates: base.html, login.html, signup.html.
    Connect urls: accounts, django.contrib.auth.
    Create SignUpView.

--- triplinker/core/urls.py
+++ triplinker/core/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+
 
 urlpatterns = [
+    path('', include('django.contrib.auth.urls')),
+    path('', include('accounts.urls')),
     path('admin/', admin.site.urls),
 ]
