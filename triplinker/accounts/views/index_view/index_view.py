b6d08832d4b2363de30745a7c871fddbd3a8c527
GonnaFlyMethod <alexandergusakov@yahoo.com>
2020-10-21 15:48:37 +0300

    Up to date with original repo

--- triplinker/accounts/views/index_view/index_view.py
+++
@@ -1,8 +0,0 @@
-# Django modules.
-from django.http import HttpResponseRedirect
-from django.urls import reverse
-
-
-def index_view(request):
-    """Rederects to the page with feed"""
-    return HttpResponseRedirect(reverse('accounts:feed'))
